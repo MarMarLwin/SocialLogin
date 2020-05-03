9a07ae42c069808769f660a108e3cdc2e1ca1f22
Mar Mar Lwin <marmarlwin.dev@gmail.com>
2020-05-03 22:37:17 +0630

    Add project files.

---
+++ SocialLogin/SocialLogin.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SocialLogin.Droid
+{
+    [Activity(Label = "SocialLogin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, savedInstanceState); // for login with google
+                                                                                                                       // User-Agent tweaks for Embedded WebViews
+            global::Xamarin.Auth.WebViewConfiguration.Android.UserAgent = "Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.135 Mobile Safari/537.36";
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ SocialLogin/SocialLogin/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Auth;
+using Xamarin.Forms;
+
+namespace SocialLogin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var authenticator = new OAuth2Authenticator
+                         (
+                           //"198222079335-ecsj26g8tc9ve335m71h83m1ver882j0.apps.googleusercontent.com",
+                           "51185825091-fgfs808gfouq54a4f25gh0b4hheuvh9d.apps.googleusercontent.com",
+                           "email profile",
+                            new System.Uri("https://accounts.google.com/o/oauth2/auth"),
+                           new System.Uri("https://www.google.com")
+                          //new System.Uri("https://www.systematic-solution.com/home")
+                          );
+
+            authenticator.AllowCancel = true;
+
+            var presenter = new Xamarin.Auth.Presenters.OAuthLoginPresenter();
+           
+            presenter.Login(authenticator);
+
+            authenticator.Completed += async (senders, obj) =>
+            {
+                if (obj.IsAuthenticated)
+                {
+                    var clientData = new HttpClient();
+
+                    //call google api to fetch logged in user profile info 
+                    var resData = await clientData.GetAsync("https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + obj.Account.Properties["access_token"]);
+                    var jsonData = await resData.Content.ReadAsStringAsync();
+
+                    // deserlize the jsondata and intilize in GoogleAuthClass
+                    GoogleAuthClass googleObject = JsonConvert.DeserializeObject<GoogleAuthClass>(jsonData);
+
+                    //you can access following property after login
+                    string email = googleObject.email;
+                    string photo = googleObject.picture;
+                    string name = googleObject.name;
+                    
+                    lblemail.Text = email;
+                    lblName.Text = name;
+                }
+                else
+                {
+                    //Authentication fail
+                    // write the code to handle when auth failed
+                }
+            };
+            authenticator.Error += onAuthError;
+        }
+        private void onAuthError(object sender, AuthenticatorErrorEventArgs e)
+        {
+            DisplayAlert("Google Authentication Error", e.Message, "OK");
+        }
+    }
+}
